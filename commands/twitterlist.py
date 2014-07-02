f50743c0b22198c425f227b3789ecb6b63603f63
Fugiman <Fugiman47@gmail.com>
2014-07-02 15:21:51 -0700

    Add .twitterlist

---
+++ commands/twitterlist.py
@@ -0,0 +1,12 @@
+config = {
+    "access": "admin",
+    "help": ".twitterlist || .twitterlist || Lists twitter accounts that can be used for Nyaa descriptions"
+}
+
+def command(guid, manager, irc, channel, user):
+    twitters = yield manager.master.modules["config"].get("nyaa", "twitter", {"jdp": "johnnydickpants"})
+    twitters = sorted(twitters.items())
+
+    while twitters:
+        o, twitters = twitters[:5], twitters[5:]
+        irc.msg(channel, u", ".join([u"{} -> @{}".format(k, v) for k, v in o]))
