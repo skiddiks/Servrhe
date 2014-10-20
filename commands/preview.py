d6e6132061112561c5afb856d3220f53bebdf3cc
Fugiman <Fugiman47@gmail.com>
2014-10-19 17:40:13 -0700

    Add experimental webm support to .preview

--- commands/preview.py
+++ commands/preview.py
@@ -5,10 +5,10 @@ import os, re
 
 config = {
     "access": "admin",
-    "help": ".preview [show] (--previous) (--preview=TIME) || .preview prince || Generates a preview image. TIME can be a frame number, HH:MM:SS.MM (must contain period), CHAPTER+SS.MM (chapter should match exactly), or FTP to force an ftp download."
+    "help": ".preview [show] (--previous) (--preview=TIME) (--webm=DURATION) || .preview prince || Generates a preview image. TIME can be a frame number, HH:MM:SS.MM (must contain period), CHAPTER+SS.MM (chapter should match exactly), or FTP to force an ftp download."
 }
 
-def command(guid, manager, irc, channel, user, show, previous = False, preview = None):
+def command(guid, manager, irc, channel, user, show, previous = False, preview = None, webm = None):
     show = manager.master.modules["showtimes"].resolve(show)
 
     if not show.folder.ftp:
@@ -19,6 +19,14 @@ def command(guid, manager, irc, channel, user, show, previous = False, preview =
 
     if preview is not None:
         preview = preview.lower()
+
+    if webm is True:
+        webm = 3.0 # Defaults to 3 seconds
+    elif webm:
+        try:
+            webm = float(webm)
+        except:
+            raise manager.exception(u"--webm must be the number of seconds the preview should last for. Got \"{}\" instead.".format(webm))
 
     folder = "{}/{:02d}".format(show.folder.ftp, episode)
     manager.dispatch("update", guid, u"Caching {}".format(folder))
@@ -48,8 +56,6 @@ def command(guid, manager, irc, channel, user, show, previous = False, preview =
         raise manager.exception(u"Aborted releasing {}: Couldn't find preview image on FTP".format(show.name.english))
 
     if not ftp or (preview is not None and preview != "ftp"):
-        preview_ext = "jpg"
-
         if preview is None or "+" in preview:
             try:
                 manager.dispatch("update", guid, u"Determining last modified xml file")
@@ -93,20 +99,47 @@ def command(guid, manager, irc, channel, user, show, previous = False, preview =
                 raise manager.exception(u"Aborted previewing {}: Requested chapter \"{}\" not found".format(show.name.english, chapter))
             time = chapters[chapter]["start"] + manager.master.modules["subs"].timeToInt(offset)
 
-        if time > 20000:
+        if webm and preview is None:
+            rough_time = manager.master.modules["subs"].intToTime(time, short=True)
+            fine_time = "0.000"
+        elif time > 20000:
             rough_time = manager.master.modules["subs"].intToTime(time - 20000, short=True)
             fine_time = "20.000"
         else:
             rough_time = "0.000"
             fine_time = manager.master.modules["subs"].intToTime(time, short=True)
 
-        extraargs = []
-        if preview is None:
-            extraargs.extend(["-vf", "select='eq(pict_type,I)'", "-vsync", "2"])
-        #extraargs.extend(["-vf", "colormatrix=bt709:bt601"])
 
         manager.dispatch("update", guid, u"Generating preview image")
-        out, err, code = yield getProcessOutputAndValue(manager.master.modules["utils"].getPath("avconv"), args=["-ss", rough_time, "-i", os.path.join(guid, premux), "-ss", fine_time] + extraargs + ["-vframes", "1", os.path.join(guid, "preview.{}".format(preview_ext))], env=os.environ)
+
+        if webm:
+            preview_ext = "webm"
+            out, err, code = yield getProcessOutputAndValue(manager.master.modules["utils"].getPath("avconv"), args=[
+                "-ss", rough_time,
+                "-i", os.path.join(guid, premux),
+                "-ss", fine_time,
+                "-t", "{:0.3f}".format(webm),
+                "-threads", "4", # Speed up encoding with 4 threads
+                "-c:v", "libvpx", # Use standard webm video codec
+                "-an", # Disable audio
+                "-b:v", "800K", # 800kbps is plenty
+                "-vf", "scale=-1:360", # 360p is plenty
+                os.path.join(guid, "preview.{}".format(preview_ext))
+            ], env=os.environ)
+        else:
+            preview_ext = "jpg"
+            extraargs = []
+            if preview is None:
+                extraargs.extend(["-vf", "select='eq(pict_type,I)'", "-vsync", "2"])
+            #extraargs.extend(["-vf", "colormatrix=bt709:bt601"])
+            out, err, code = yield getProcessOutputAndValue(manager.master.modules["utils"].getPath("avconv"), args=[
+                "-ss", rough_time,
+                "-i", os.path.join(guid, premux),
+                "-ss", fine_time
+            ] + extraargs + [
+                "-vframes", "1",
+                os.path.join(guid, "preview.{}".format(preview_ext))
+            ], env=os.environ)
 
         if code != 0:
             manager.log(out)
